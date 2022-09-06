88a33bf423dd8a4ed019e853bfcb06a425066e8a
Andrew A <tov_mayor@mail.ru>
2022-09-06 19:33:36 +0300

    adding SA & bucket

--- config.tf
+++ config.tf
@@ -36,12 +36,35 @@ provider "yandex" {
   zone      = "ru-central1-b"
 }
 
-resource "yandex_storage_object" "war-bucket" {
-  access_key = "YCAJECaxhWcm6rHMYeehDO2kH"
-  secret_key = "YCPxhl-X6BvNYbA6cSF5Wpr8TlRHxBhkV6lhHz5S"
-  bucket     = "devbucket"
-  key        = "folder"
-  source     = "/home/andrew/terraform-yc/hello-1.0.war"
+# resource "yandex_storage_object" "war-bucket" {    #!!NOT WORKS!!
+#   access_key = "YCAJECaxhWcm6rHMYeehDO2kH"
+#   secret_key = "YCPxhl-X6BvNYbA6cSF5Wpr8TlRHxBhkV6lhHz5S"
+#   bucket     = "devbucket"
+#   key        = "folder"
+#   source     = "/home/andrew/terraform-yc/hello-1.0.war"
+# }
+
+resource "yandex_iam_service_account" "sa" {
+  name = "bucket-admin"
+}
+
+// Назначение роли сервисному аккаунту
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  role   = "storage.editor"
+  member = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Создание бакета с использованием ключа
+resource "yandex_storage_bucket" "test" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "dev-bucket"
 }
 
 resource "yandex_compute_instance" "build" {
