3fcfbcc6dbccc133ac6c4400b7cac9384ab1682e
Andrew A <tov_mayor@mail.ru>
2022-09-06 15:54:48 +0300

    dev vm

--- config.tf
+++ config.tf
@@ -67,7 +67,7 @@ network_interface {
   }
 provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update -y && apt-get install -y maven git",
+      "sudo apt-get update -y && sudo apt-get install -y maven git",
       "sudo git clone https://github.com/tovmayor/myboxfuse.git",
       "sudo mvn -f ./myboxfuse package", 
     ]
@@ -79,3 +79,49 @@ provisioner "remote-exec" {
     }
   }
 }
+
+# resource "yandex_compute_instance" "prod" { 
+#   name        = "t-prod1"
+#   hostname    = "t-prod1"
+#   platform_id = "standard-v1"
+#   zone        = "ru-central1-b"
+#   scheduling_policy {
+#     preemptible = true
+#   }
+#   allow_stopping_for_update = true
+
+#   resources {
+#     cores  = 2
+#     memory = 2
+#   }
+
+#   boot_disk {
+#     initialize_params {
+#       image_id = "fd8kdq6d0p8sij7h5qe3"
+#       size = "10"
+#     }
+#   }
+# network_interface {
+#     subnet_id = "e2lbocl9ri9asmv0hq7i"
+#     nat = true
+#   }
+
+#   metadata = {
+#     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+#   }
+# provisioner "remote-exec" {
+#     inline = [
+#       "RUN apt-get install -y openjdk-11-jre-headless"
+#       "RUN wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz"
+#       "RUN tar xvfz apache-tomcat-9.0.65.tar.gz"
+#       "RUN mkdir -p /usr/local/tomcat9 && mv ./apache-tomcat-9.0.65/* /usr/local/tomcat9 && rm apache-tomcat-9.0.65.tar.gz"
+
+#     ]
+#     connection {
+#       type = "ssh"
+#       user = "ubuntu"
+#       private_key = file("~/.ssh/id_rsa")
+#       host = self.network_interface[0].nat_ip_address
+#     }
+#   }
+# }
