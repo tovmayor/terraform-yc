6dc198cf0dfc53ebc662b48049cc4edd11416484
Andrew A <tov_mayor@mail.ru>
2022-09-06 15:59:59 +0300

    fix core fraction + prod vm

--- config.tf
+++ config.tf
@@ -44,6 +44,7 @@ resource "yandex_compute_instance" "build" {
   scheduling_policy {
     preemptible = true
   }
+  core_fraction = 20
   allow_stopping_for_update = true
 
   resources {
@@ -80,48 +81,49 @@ provisioner "remote-exec" {
   }
 }
 
-# resource "yandex_compute_instance" "prod" { 
-#   name        = "t-prod1"
-#   hostname    = "t-prod1"
-#   platform_id = "standard-v1"
-#   zone        = "ru-central1-b"
-#   scheduling_policy {
-#     preemptible = true
-#   }
-#   allow_stopping_for_update = true
+resource "yandex_compute_instance" "prod" { 
+  name        = "t-prod1"
+  hostname    = "t-prod1"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-b"
+  scheduling_policy {
+    preemptible = true
+  }
+  core_fraction = 20
+  allow_stopping_for_update = true
 
-#   resources {
-#     cores  = 2
-#     memory = 2
-#   }
+  resources {
+    cores  = 2
+    memory = 2
+  }
 
-#   boot_disk {
-#     initialize_params {
-#       image_id = "fd8kdq6d0p8sij7h5qe3"
-#       size = "10"
-#     }
-#   }
-# network_interface {
-#     subnet_id = "e2lbocl9ri9asmv0hq7i"
-#     nat = true
-#   }
+  boot_disk {
+    initialize_params {
+      image_id = "fd8kdq6d0p8sij7h5qe3"
+      size = "10"
+    }
+  }
+network_interface {
+    subnet_id = "e2lbocl9ri9asmv0hq7i"
+    nat = true
+  }
 
-#   metadata = {
-#     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-#   }
-# provisioner "remote-exec" {
-#     inline = [
-#       "RUN apt-get install -y openjdk-11-jre-headless"
-#       "RUN wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz"
-#       "RUN tar xvfz apache-tomcat-9.0.65.tar.gz"
-#       "RUN mkdir -p /usr/local/tomcat9 && mv ./apache-tomcat-9.0.65/* /usr/local/tomcat9 && rm apache-tomcat-9.0.65.tar.gz"
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+provisioner "remote-exec" {
+    inline = [
+      "RUN apt-get install -y openjdk-11-jre-headless"
+      "RUN wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz"
+      "RUN tar xvfz apache-tomcat-9.0.65.tar.gz"
+      "RUN mkdir -p /usr/local/tomcat9 && mv ./apache-tomcat-9.0.65/* /usr/local/tomcat9 && rm apache-tomcat-9.0.65.tar.gz"
 
-#     ]
-#     connection {
-#       type = "ssh"
-#       user = "ubuntu"
-#       private_key = file("~/.ssh/id_rsa")
-#       host = self.network_interface[0].nat_ip_address
-#     }
-#   }
-# }
+    ]
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host = self.network_interface[0].nat_ip_address
+    }
+  }
+}
