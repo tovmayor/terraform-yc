45d646035b21668facd68e55593e153dac67fb05
Andrew A <tov_mayor@mail.ru>
2022-09-06 19:36:44 +0300

    folder_id to SA

--- config.tf
+++ config.tf
@@ -52,6 +52,7 @@ resource "yandex_iam_service_account" "sa" {
 resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
   role   = "storage.editor"
   member = "serviceAccount:${yandex_iam_service_account.sa.id}"
+  folder_id = "b1gqgtdu7assr55vqtf2"
 }
 
 // Создание статического ключа доступа
