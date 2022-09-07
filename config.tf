768519043168c692660ed29603f3bd375cdf3ffa
Andrew A <tov_mayor@mail.ru>
2022-09-07 21:29:30 +0300

    123

--- config.tf
+++ config.tf
@@ -12,7 +12,7 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "y0_AgAAAAAKYyQGAATuwQAAAADNneRzsw53q7T1Q2qTJPmxqfz87uq9uBk"
+  token     = "y0_"
   cloud_id  = "b1g2mjplbcl08o830ovt"
   folder_id = "b1gqgtdu7assr55vqtf2"
   zone      = "ru-central1-b"
